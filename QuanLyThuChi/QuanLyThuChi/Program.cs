0fcd15c72e639c34bfc0974b24814cb02b0bebc1
DuongNguyenHoangThuy <42516808+DuongNguyenHoangThuy@users.noreply.github.com>
2018-10-28 19:45:59 +0700

    ddđ

--- QuanLyThuChi/QuanLyThuChi/Program.cs
+++ QuanLyThuChi/QuanLyThuChi/Program.cs
@@ -21,7 +21,7 @@ namespace QuanLyThuChi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new LogIn());
         }
     }
 }
